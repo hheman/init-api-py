656124eb03e6b6c8e3e4000fda21257ed5285cb8
Himanish <himanish.kaushal@gmail.com>
2024-02-07 14:40:57 -0500

    update deploy script to name docker container and stop it before rebuilding

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 git checkout master
 git pull
-docker build -t init-api-py .
-docker run -dp 80:80 init-api-py
+docker stop init-api-py-ctnr
+docker build -t init-api-py-img .
+docker run --name init-api-py-ctnr -dp 80:80 init-api-py-img
