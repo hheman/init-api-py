c82a9737b1d46c4c054fd08cb9febe3ab79a529f
Himanish <himanish.kaushal@gmail.com>
2024-02-07 14:58:57 -0500

    updating delpoy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 git checkout master
 git pull
+echo "Stopping docker container ..."
 docker stop init-api-py-ctnr
+echo "... stopped"
+sleep 5s
+
+echo "Removing docker container ..."
+docker rm -f init-api-py-ctnr
+echo "... removed"
+sleep 5s
+
+echo "Building docker image ..."
 docker build -t init-api-py-img .
+
+echo "Creating docker container ..."
 docker run --name init-api-py-ctnr -dp 80:80 init-api-py-img
