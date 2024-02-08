1e61578560527041fc4bb8ca74665ed20ebe6dd8
Himanish <himanish.kaushal@gmail.com>
2024-02-08 17:27:00 -0500

    add docker compose

--- app/database.py
+++ app/database.py
@@ -4,9 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://postgres:{os.getenv('DB_PASSWORD')}@localhost:54320/initapipy"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(os.getenv('DATABASE_URL'))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
