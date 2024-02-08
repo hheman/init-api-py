5eac19ec4430f7ce5b63bb6c9bddc9123364043b
Himanish <himanish.kaushal@gmail.com>
2024-02-07 20:08:19 -0500

    add db pwd env var to docker build

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ echo "... removed"
 sleep 5s
 
 echo "Building docker image ..."
-docker build -t init-api-py-img .
+docker build -t init-api-py-img --build-arg db_password=$DB_PASSWORD .
 
 echo "Creating docker container ..."
 docker run --name init-api-py-ctnr -dp 80:80 init-api-py-img
