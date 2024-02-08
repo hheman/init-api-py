2f08f53929c14f771e63086c7af078a3ba64736d
Himanish <himanish.kaushal@gmail.com>
2024-02-08 18:03:16 -0500

    env vars and docker compose updates

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,9 @@
 git checkout master
 git pull
-echo "Stopping docker container ..."
-docker stop init-api-py-ctnr
-echo "... stopped"
+
+echo "Rebuilding ..."
+docker-compose down
 sleep 5s
-
-echo "Removing docker container ..."
-docker rm -f init-api-py-ctnr
-echo "... removed"
-sleep 5s
-
-echo "Building docker image ..."
-docker build -t init-api-py-img --build-arg db_password=$DB_PASSWORD .
-
-echo "Creating docker container ..."
-docker run --name init-api-py-ctnr -dp 80:80 init-api-py-img
+docker-compose build
+docker-compose up -d
+echo "... done"
