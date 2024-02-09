150f8057d2f8cd2ff942f586f02840337bfe5bc4
Himanish <himanish.kaushal@gmail.com>
2024-02-08 21:32:12 -0500

    install docker-compose during instance setup

--- instance-setup.sh
+++ instance-setup.sh
@@ -2,3 +2,6 @@ sudo yum update -y
 sudo yum -y install docker git
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
